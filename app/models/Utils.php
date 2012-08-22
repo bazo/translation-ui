dfa675d58723e3600ca16d0bcea8e34920a8a12c
Martin Bazik <martin.bazik@digitask.sk>
2012-08-22 16:25:28 +0200

    fixed immport

---
+++ app/models/Utils.php
@@ -0,0 +1,15 @@
+<?php
+namespace Helpers;
+use Nette\Utils\Strings;
+/**
+ * Utils
+ *
+ * @author martin.bazik
+ */
+class Message
+{
+	public static function encodeMessageId($messageId)
+	{
+		return Strings::replace($messageId, '/\./', 'DOT');
+	}
+}
