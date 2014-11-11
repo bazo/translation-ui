e12c530fffb950d6cd65a7bd221d164e969f3a6a
bazo <martin@bazo.sk>
2014-11-11 11:54:17 +0100

    api: download translation files

--- app/ApiModule/presenters/ProjectsPresenter.php
+++ app/ApiModule/presenters/ProjectsPresenter.php
@@ -2,6 +2,10 @@
 
 namespace ApiModule;
 
+
+use KdybyTranslationBuilder;
+use Nette\Neon\Neon;
+
 /**
  */
 class ProjectsPresenter extends BasePresenter
@@ -9,6 +13,9 @@ class ProjectsPresenter extends BasePresenter
 
 	/** @var \Facades\Project  @inject */
 	public $projectFacade;
+
+	/** @var \Facades\Translation @inject */
+	public $translationFacade;
 
 	protected function startup()
 	{
@@ -26,7 +33,7 @@ class ProjectsPresenter extends BasePresenter
 
 		if ($hash === $verifyHash) {
 			$messageData = unserialize($data);
-			$imported = $this->projectFacade->importTemplate($messageData, $project);
+			$imported	 = $this->projectFacade->importTemplate($messageData, $project);
 			$this->sendResponse(new \Nette\Application\Responses\JsonResponse(['error' => 'false', 'message' => sprintf('OK. Imported %d messages.', $imported)]));
 		} else {
 			$this->sendResponse(new \Nette\Application\Responses\JsonResponse(['error' => 'true', 'message' => 'Bad request. Hash does not match.']));
@@ -36,5 +43,45 @@ class ProjectsPresenter extends BasePresenter
 	}
 
 
-}
+	public function actionDownloadTranslations($id, $hash)
+	{
+		$project = $this->projectFacade->find($id);
 
+		$verifyHash = hash_hmac('sha256', $id, $project->getKey());
+
+		if ($hash === $verifyHash) {
+			$this->buildFiles($project);
+		} else {
+			$this->sendResponse(new \Nette\Application\Responses\JsonResponse(['error' => 'true', 'message' => 'Bad request. Hash does not match.']));
+		}
+
+		$this->terminate();
+	}
+
+
+	private function buildFiles(\Project $project)
+	{
+		$translations = $project->getTranslations();
+
+		$builder = new KdybyTranslationBuilder;
+
+		$files = [];
+
+		foreach ($translations as $translation) {
+			$mask	 = '%s.' . $translation->getLocale() . '.neon';
+			$dictionaryData	 = $this->translationFacade->getDictionaryData($translation);
+			$outputFiles	 = $builder->build($mask, $dictionaryData, 'messages');
+			$files			 = array_merge($files, $outputFiles);
+		}
+
+		$zip = new \ZipStream(sprintf('%s.zip', $project->getName()));
+
+		foreach ($files as $fileName => $data) {
+			$data = Neon::encode(current($files), Neon::BLOCK);
+			$zip->add_file($fileName, $data);
+		}
+		$zip->finish();
+	}
+
+
+}
